93dc48c610150a7bd3d560f456eadcbfe0c545ea
syj6527-oss <syj6527@gmail.com>
2026-03-05 02:35:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -206,23 +206,27 @@ async function processMessage(id, isInput = false) {
             const targetEl = editArea[0];
             
             // 🔥 핵심: 예전 저장본 말고 '현재 텍스트박스 안에 적힌 글씨'를 긁어온다!
-            let textToTranslate = targetEl.value.trim(); 
-            const sourceLang = detectLang(textToTranslate);
+           let textToTranslate = targetEl.value.trim();
+
+const sourceLang = detectLang(textToTranslate);
+
 if (isTranslatingInput || !textToTranslate) return;
+
 const targetLang = sourceLang === "ko"
-    ? "en"
-    : "ko";
-            
-            if (textToTranslate && textToTranslate !== "") {
-                catNotify("🐱 수정창 텍스트 번역 중...", "success"); 
-                
-                const translated = await fetchTranslation(
-  textToTranslate,
-  isInput,
-  null,
-  sourceLang,
-  targetLang
-);
+  ? "en"
+  : "ko";
+
+if (textToTranslate && textToTranslate !== "") {
+  catNotify("😺 수정창 텍스트 번역 중...", "success");
+
+  const translated = await fetchTranslation(
+    textToTranslate,
+    isInput,
+    null,
+    sourceLang,
+    targetLang
+  );
+}
                 
                 if (translated && translated !== textToTranslate) {
                     // 번역된 텍스트를 창에 꽂아넣기
