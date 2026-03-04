0728a4de62f1f418e18e267a32ca679c8ca4f89c
syj6527-oss <syj6527@gmail.com>
2026-03-05 03:26:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -332,7 +332,6 @@ let targetEl = editArea.length
 
 console.log("TARGET:", targetEl);
 
-let textToTranslate = targetEl.value.trim();
 console.log("INPUT TEXT:", textToTranslate); // 메인 입력창 글씨 바로 긁어오기!
 
 const sourceLang = detectLang(textToTranslate);
